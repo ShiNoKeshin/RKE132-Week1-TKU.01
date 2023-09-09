797de7a1639e006105994d97c4dd302fa3c92f7e
Tonis Kuningas <tonis.kuningas@gmail.com>
2023-09-09 14:34:10 +0300

    create new app

---
+++ My1stApp/My1stApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// See on lisainfoks - kõigest kommentaar.
+// Console.WriteLine("See ei ole kommentaar.");
+// Rakendus vajab kasutaja sisestatud nime.
+Console.WriteLine("Enter name!"); // output
+// string userName;
+// userName = Console.ReadLine();
+string userName = Console.ReadLine(); // input
+
+Console.WriteLine("Jello, " + userName + "!"); // output
+// string interpolation - $ , {} - sulgude sees muutuja
+Console.WriteLine($"Jello, {userName}!"); // output
