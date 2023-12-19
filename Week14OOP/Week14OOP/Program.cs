e2c86c70e34f129277e3f2b225ad1e5aeddef48d
Tonis Kuningas <tonis.kuningas@gmail.com>
2023-12-20 01:18:08 +0200

    added week 14

---
+++ Week14OOP/Week14OOP/Program.cs
@@ -0,0 +1,73 @@
+﻿Console.WriteLine("Name the god:");
+string myDogName = Console.ReadLine();
+Dog myDog = new Dog(myDogName);
+
+Dog stillMyDog = new Dog("Chester");
+
+Console.WriteLine($"{myDog.Name} the dog name.");
+Console.WriteLine($"{stillMyDog.Name} is also dog.");
+
+myDog.Rename("Douggie");
+
+while (myDog.LevelOfWag != 5)
+{
+    myDog.Bork();
+}
+
+myDog.WagBoy();
+
+while (myDog.LevelOfBork != 5)
+{
+    myDog.WagBoy();
+}
+
+myDog.Bork();
+
+class Dog
+{
+    private string _name;
+    private int _levelOfWag;
+    private int _levelOfBork;
+
+    //constructor
+    public Dog(string name) //name - name the dog
+    {
+        _name = name;
+        _levelOfWag = 0;
+    }
+
+    //getter
+    public string Name
+    { get 
+        { return _name; } 
+    }
+
+    public int LevelOfWag
+    {
+        get { return _levelOfWag; }
+    }
+
+    public int LevelOfBork
+    {
+        get { return _levelOfBork; }
+    }
+
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"An dog has been renamed to {newName}.");
+    }
+
+    public void Bork()
+    {
+        Console.WriteLine("Woofer bork");
+        _levelOfWag++;
+    }
+
+    public void WagBoy()
+    {
+        Console.WriteLine("Waggle wiggle-");
+        _levelOfBork++;
+    }
+
+}
